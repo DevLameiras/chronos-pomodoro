d6a67c9eb42118feefe1799584eea9ff709e3e08
DevLameiras <umgansoqualquer@gmail.com>
2026-01-19 21:31:45 -0300

    Criei css modules para isolar um h1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Heading } from './components/Heading';
+
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
   return (
     <>
-      <h1>Ola mundo!</h1>
+      <Heading />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum sequi
         magni sapiente minima, unde cumque voluptates. Quae minus excepturi
