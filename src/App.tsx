dc31b81366de3be624511af015b4dcce8ec73d11
DevLameiras <umgansoqualquer@gmail.com>
2026-02-05 09:53:24 -0300

    Finalizando o input e criando o componente Cycles com indicadores de ciclo pomodoro em cores.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput/Index';
+import { Cycles } from './components/Cycles/Index';
 
 export function App() {
   return (
@@ -24,7 +25,12 @@ export function App() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-            <DefaultInput id='MeuInput' type='text' />
+            <DefaultInput
+              labelText='QualquerCoisa'
+              id='MeuInput'
+              type='text'
+              placeholder='Digite algo'
+            />
           </div>
 
           <div className='formRow'>
@@ -32,8 +38,7 @@ export function App() {
           </div>
 
           <div className='formRow'>
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0 0</p>
+            <Cycles />
           </div>
 
           <div className='formRow'>
