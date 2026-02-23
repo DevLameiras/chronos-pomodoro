476ecb6bd4cfd4ad2f1a0a18200bcf8de3a5cf5c
DevLameiras <umgansoqualquer@gmail.com>
2026-02-23 18:14:44 -0300

    Estado global da aplicação com useState e comunicação via props.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,36 @@ import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { useState } from 'react';
+import type { TaskStateModel } from './models/TaskStateModel';
 
+// export type TaskStateModel = {
+//   tasks: [TaskModel];
+//   secondsRemaining: number;
+//   formattedSecondsRemaining: string;
+//   activeTask: TaskModel | null;
+//   currentyCycle: number; // 1 a 8
+//   config: {
+//     workTime: number;
+//     shortBreaktime: number;
+//     longBreakTime: number;
+//   };
+// }
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentyCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreaktime: 5,
+    longBreakTime: 15,
+  },
+};
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+
+  return <Home state={state} setState={setState} />;
 }
