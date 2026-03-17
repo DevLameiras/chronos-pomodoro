bd3f29d1c0ae53b32d8c06ecba157dc8bb47f870
DevLameiras <umgansoqualquer@gmail.com>
2026-03-17 17:33:12 -0300

    Movendo o estado real para dentro do contexto.

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,28 @@ import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
 import { useState } from 'react';
-import type { TaskStateModel } from './models/TaskStateModel';
+import { TaskStateModel } from './models/TaskStateModel';
+import { TaskContextProvider } from './contexts/TaskContext';
 
 const initialState: TaskStateModel = {
   tasks: [],
   secondsRemaining: 0,
   formattedSecondsRemaining: '00:00',
   activeTask: null,
-  currentyCycle: 0,
+  currentCycle: 0,
   config: {
     workTime: 25,
-    shortBreaktime: 5,
+    shortBreakTime: 5,
     longBreakTime: 15,
   },
 };
+
 export function App() {
   const [state, setState] = useState(initialState);
 
-  return <Home />;
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
